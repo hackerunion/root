95ce1a09bb1dc6c58fde694f0f28d49507aed9d2
Brandon Diamond <brandon.t.diamond@gmail.com>
2015-02-25 22:47:58 -0500

    Some progress on sbin

---
+++ lib/sh/common.sh
@@ -0,0 +1,22 @@
+# Helper utilities for working with system files
+
+field() {
+  JSON="$1"
+	ENTRY="$2"
+	FIELD="$3"
+	echo "$JSON" | grep -i "\[$ENTRY,\"$FIELD\"\]" | cut -f 2 | sed -e 's/^"\(.*\)"$/\1/'
+}
+
+fields() {
+  echo "$1" | cut -f 1 | tr -d ',[]"0-9' | sort | uniq -c
+}
+
+last() {
+  JSON="$1"
+	l=`echo "$JSON" | cut -d',' -f1 | cut -c2- | sort -rn | head -1`
+	echo ${l:--1}
+}
+
+bool() {
+	[ -n "$1" ] && [ "$1" != "false" ] && echo "true"
+}
