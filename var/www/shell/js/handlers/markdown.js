c439996b46353e47c93c151b4e43dd8f8940dc79
Brandon Diamond <brandon.t.diamond@gmail.com>
2015-04-20 21:28:53 -0400

    +

--- var/www/shell/js/handlers/markdown.js
+++ var/www/shell/js/handlers/markdown.js
@@ -1,6 +1,6 @@
 function(err, ctx, uri, opts, cb) {
   Kernel.withScripts(['/usr/lib/js/marked/dev.js'], function() {
-    opts['markup'] = marked(opts.data);
+    opts['markup'] = marked(opts.data, { 'smartypants': true });
     return cb(err, ctx, uri, opts);
   });
 }
