d9e623f537d4696c82e4905975e6115584a4d271
root <root@ip-172-31-30-92.us-west-2.compute.internal>
2015-07-19 03:54:01 +0000

    changes from ip-172-31-30-92 on Sun Jul 19 03:54:01 UTC 2015

--- home/server/sandbox/stack/js/app.js
+++ home/server/sandbox/stack/js/app.js
@@ -1,55 +1,80 @@
 $(function() {
-////////////////////
-////////////////////
-////////////////////
+  var intersectPoint = function(X, Y, x, y, w, h) {
+    return X >= x && X <= (x + w) && Y >= y && Y <= (y + h);
+  };
+  
+  var intersectMiddle = function(X, Y, W, H, x, y, w, h) {
+    return X >= x && X <= (x + w) && ((Y >= y && Y <= (y + h)) || (y >= Y && y <= (Y + H)));
+  };
 
-// x1, y1 would be mouse coordinates onmousedown
-// x2, y2 would be mouse coordinates onmouseup
-// all coordinates are considered relative to the document
-function rectangleSelect(selector, sx, sy, ex, ey) {
-  var elements = [];
-    
-  x1 = Math.min(sx, ex);
-  y1 = Math.min(sy, ey);
-  x2 = Math.max(sx, ex);
-  y2 = Math.max(sy, ey);
+  var intersectOne = function(X, Y, W, H, x, y, w, h) {
+    return intersectPoint(X + 0, Y + 0, x, y, w, h) ||
+           intersectPoint(X + W, Y + 0, x, y, w, h) ||
+           intersectPoint(X + W, Y + H, x, y, w, h) ||
+           intersectPoint(X + 0, Y + H, x, y, w, h) ||
+           intersectMiddle(X, Y, W, H, x, y, w, h);
+  };
+  
+  var intersect = function(X, Y, W, H, x, y, w, h) {
+    return intersectOne(X, Y, W, H, x, y, w, h) ||
+           intersectOne(x, y, w, h, X, Y, W, H);
+  };
+  
+  var rectangleSelect = function(selector, sx, sy, ex, ey, strict) {
+    var elements = [];
+      
+    x1 = Math.min(sx, ex);
+    y1 = Math.min(sy, ey);
+    x2 = Math.max(sx, ex);
+    y2 = Math.max(sy, ey);
 
-  $(selector).each(function() {
-    var $this = $(this);
-    var offset = $this.offset();
-    var x = offset.left;
-    var y = offset.top;
-    var w = $this.width();
-    var h = $this.height();
+    $(selector).each(function() {
+      var $this = $(this);
+      var offset = $this.offset();
+      var x = offset.left;
+      var y = offset.top;
+      var w = $this.width();
+      var h = $this.height();
+      var overlap = !strict && intersect(x1, y1, x2-x1, y2-y1, x, y, w, h);
+  
+      if (overlap || (
+        x >= x1 &&
+        y >= y1 && 
+        x + w <= x2 &&
+        y + h <= y2)) {
+        elements.push($this.get(0));
+        return true;
+      }
+    });
 
-    if (x >= x1 
-      && y >= y1 
-      && x + w <= x2 
-      && y + h <= y2) {
-      // this element fits inside the selection rectangle
-      elements.push($this.get(0));
-    }
-  });
-  return elements;
-}
-
-////////////////////
-////////////////////
-////////////////////
+    return elements;
+  }
 
   var navigate = function(stack, go) {
+    console.log("GO:", go);
     if (go in stack.cards) {
       // render card + push history
     }
 
     // redirect to web url
   };
+  
+  var is_color = function(s) {
+    return /^#([a-f0-9]{3}|[a-f0-9]{6})$/i.test(s);
+  };
 
-  var renderCard = function($p, stack, card){
-    var $card = $("<table />").addClass("card");
+  var renderCard = function($s, stack, card){
     var $tr;
+    var $card = $("<table />").addClass("card");
+    var handler = function(r, c, cell) {
+      return function() {
+        if (!isEditMode($s) && cell.go) {
+          navigate(stack, cell.go);
+        }
+      }
+    };
 
-    for(var r=0; r<card.data.length; r++) {
+    for(var r=0; r < card.data.length; r++) {
       $tr = $("<tr />").appendTo($card);
 
       for(var c=0; c<card.data[r].length; c++) {
@@ -63,13 +88,9 @@ function rectangleSelect(selector, sx, sy, ex, ey) {
             'color': cell.fg || '#000'
           })
           .addClass(cell.go ? 'go' : '')
-          .click(function() {
-            if (cell.go) {
-              navigate(stack, cell.go);
-            }
-
-            return false;
-          });
+          .data('cell-col', c)
+          .data('cell-row', r)
+          .click(handler(r, c, cell));
 
         if (cell.f) {
           cell.f($cell, cell, $card, card);
@@ -77,18 +98,38 @@ function rectangleSelect(selector, sx, sy, ex, ey) {
       }
     }
 
-    $p.empty().append($card);
+    $s.empty().append($card);
 
     if (card.f) {
       card.f($card, card);
     }
   };
 
-  var renderStack = function($p, stack) {
-    renderCard($p, stack, stack.cards[stack.home]);
+  var renderStack = function($s, stack) {
+    renderCard($s, stack, stack.cards[stack.card]);
 
     if (stack.f) {
-      stack.f($p, stack);
+      stack.f($s, stack);
+    }
+  };
+
+  var repaint = function($s, $t, stack) {
+    var is_edit = isEditMode($s);
+
+    toggleMode($s, $t, stack, is_edit);
+    renderCard($s, stack, stack.cards[stack.card]);
+    toggleMode($s, $t, stack, is_edit);
+  };
+
+  var toggleMode = function($s, $t, stack, allow) {
+    if (allow === false) {
+      return;
+    }
+    
+    if (isEditMode($s)) {
+      enableView($s, $t, stack);
+    } else {
+      enableEdit($s, $t, stack);
     }
   };
   
@@ -114,6 +155,7 @@ function rectangleSelect(selector, sx, sy, ex, ey) {
     return {
       'name': name,
       'home': home,
+      'card': home,
       'cards': _.reduce(cards, function(r, c) { r[c.name] = c; return r; }, {}),
       'f': f || null
     };
@@ -127,16 +169,30 @@ function rectangleSelect(selector, sx, sy, ex, ey) {
     return JSON.parse(s);
   };
 
-  var enableEdit = function($s, $t) {
+  var enableEdit = function($s, $t, stack) {
     var sx, sy, ex, ey, drag;
     var clear = function() { 
-      $('.cell', $s).removeClass('active selected');
+      $('.active, .selected, .cursor, .mark', $s).removeClass('active selected cursor mark');
+      $s.removeClass('selected cursor');
+    };
+
+    // Avoid unsaved changes
+    window.onbeforeunload = function(e) {
+      e = e || window.event;
+      e.preventDefault = true;
+      e.cancelBubble = true;
+      e.returnValue = 'You have unsaved changes!';
     };
 
     $s.addClass("edit");
     
     $('.cell', $s).on('click', function(e) {
       clear();
+    });
+    
+    $('.cell', $s).on('dblclick', function(e) {
+      clear();
+      $(this).addClass('mark').add($s).addClass('cursor');
     });
 
     $s.on('mousedown', function(e) {
@@ -167,32 +223,249 @@ function rectangleSelect(selector, sx, sy, ex, ey) {
       drag = false;
       
       $('.cell', $s).removeClass('active');
-
       rectangleSelect('.cell', sx, sy, ex, ey).forEach(function(e) {
-        $(e).addClass('selected');
+        $(e).add($s).addClass('selected');
       });
+    });
+    
+    $s.on('keydown', function(e) {
+      var $active = getActive($s);
+
+      if ($active && $active.length == 1) {
+        var loc = getLocation($active);
+        switch(e.which) {
+          case 8: // backspace
+          case 37: // left
+            getCell($s, loc, -1).dblclick();
+            return false;
+          case 38: // up
+            getCell($s, loc, 0, -1).dblclick();
+            return false;
+          case 39: // right
+            getCell($s, loc, 1).dblclick();
+            return false;
+          case 13: // enter
+          case 40: // down
+            getCell($s, loc, 0, 1).dblclick();
+            return false;
+        }
+      }
+    });
+
+    $s.on('keypress', function(e) {
+      var letter = String.fromCharCode(e.charCode);
+      var $active = getActive($s);
+      
+      if ($active) {
+        $active.text(letter);
+
+        getActiveData($s, stack).forEach(function(data) {
+          data.val = letter;
+        });
+        
+        if ($active.length == 1) {
+          $active.next().dblclick();
+        }
+      }
+
+      // prevent scrolling via space
+      if (letter == " ") {
+        return false;
+      }
     });
   };
 
-  var enableView = function($s, $t) {
-    $s.removeClass("edit");
-    $s.off('mousedown mouseup mousemove click');
-    $('.cell', $s).off('click');
+  var countPrevious = function($e) {
+    var i = 0;
+
+    do {
+      $e = $e.prev();
+    } while($e.length && ++i);
+    
+    return i;
+  }
+
+  var getLocation = function($c) {
+    var x = $c.data('cell-col') || countPrevious($c);
+    var y = $c.data('cell-row') || countPrevious($c.parent());
+    return [x, y];
+  };
+  
+  var getCell = function($s, loc, x, y) {
+    var $tr = $('tr:nth-child(' + (loc[1] + 1 + (y || 0)) + ')', $s);
+    return $('.cell:nth-child(' + (loc[0] + 1 + (x || 0)) + ')', $tr);
+  };
+  
+  var getDataAt = function(stack, loc) {
+    return stack.cards[stack.card].data[loc[1]][loc[0]];
   };
 
-  var enableTools = function($s, $t) {
+  var getActiveData = function($s, stack) {
+    var $active = getActive($s);
+    var result = [];
+
+    if (!$active) {
+      return result;
+    }
+
+    $active.each(function() {
+      result.push(getDataAt(stack, getLocation($(this))));
+    })
+
+    return result;
+  };
+
+  var getActive = function($s) {
+      if ($s.hasClass('selected')) {
+        return $s.find('.selected.cell');
+      }
+      
+      if ($s.hasClass('cursor')) { 
+        return $s.find('.cursor.cell');
+      }
+
+      return null;
+  };
+
+  var enableView = function($s, $t, stack) {
+    $s.removeClass("edit");
+    $s.off('mousedown mouseup mousemove click keydown keypress');
+  };
+
+  var isEditMode = function($s) {
+    return $s.hasClass('edit');
+  };
+
+  var enableTools = function($s, $t, stack) {
     $("[name=mode]", $t).change(function() {
       var edit = $(this).val() == "edit";
       
       if (edit) {
-        enableEdit($s, $t);
+        enableEdit($s, $t, stack);
         return;
       }
       
-      enableView($s, $t);
+      enableView($s, $t, stack);
+    });
+
+    $('#setfg', $t).click(function() {
+      var $active = getActive($s);
+      var fg = $("[name=fg]", $t).val().trim();
+      
+      if (is_color(fg) && $active) {
+        $active.css("color", fg);
+
+        getActiveData($s, stack).forEach(function(data) {
+          data.fg = fg;
+        });
+      }
+
+      return false;
+    });
+
+    $('#setbg', $t).click(function() {
+      var $active = getActive($s);
+      var bg = $("[name=bg]", $t).val().trim();
+      
+      if (is_color(bg) && $active) {
+        $active.css("background", bg);
+
+        getActiveData($s, stack).forEach(function(data) {
+          data.bg = bg;
+        });
+      }
+
+      return false;
+    });
+
+    $('#setgo', $t).click(function() {
+      var $active = getActive($s);
+      var go = $("[name=go]", $t).val().trim();
+      
+      if ($active) {
+        if (go) {
+          $active.addClass("go");
+        } else {
+          $active.removeClass("go");
+        }
+
+        getActiveData($s, stack).forEach(function(data) {
+          data.go = go;
+        });
+      }
+
+      return false;
+    });
+
+    $('#clipboard', $t).click(function() {
+      var data = prompt("Copy/paste raw text here:", getString($s, stack));
+
+      if (!$('.cell.cursor', $s).length) {
+        $('.cell', $s).first().dblclick();
+      }
+      
+      putString($s, data);
     });
     
+    $('#shift', $t).click(function() {
+      var $active = getActive($s);
+
+      var co = parseInt($('[name=x]', $t).val());
+      var ro = parseInt($('[name=y]', $t).val());
+  
+      var data = stack.cards[stack.card].data;
+
+      var loc1 = getLocation($active.first());
+      var loc2 = getLocation($active.last());
+
+      var c1 = loc1[0], r1 = loc1[1], c2 = loc2[0], r2 = loc2[1];
+
+      var w = c2 - c1 + 1;
+      var h = r2 - r1 + 1;
+
+      //  ensure that offset is positive
+      co = (w + (co % w)) % w;
+      ro = (h + (ro % h)) % h;
+      
+
+      repaint($s, $t, stack);
+    });
+
     $("[name=mode][value=view]").change();
+  };
+
+  var getString = function($s, stack) {
+    var s = "";
+    var data = stack.cards[stack.card].data;
+
+    for (var r=0; r<data.length; r++) {
+      for (var c=0; c<data[r].length; c++) {
+        s += data[r][c].val || ' ';
+      }
+      s += '\n';
+    }
+
+    return s.slice(0, -1);
+  };
+
+  var putString = function($s, data) {
+    data = data || "";
+
+    for(var m=0, i=0; i<data.length; m++, i++) {
+      if (data[i] == '\n') {
+        while(m--) {
+          $s.trigger($.Event('keydown', { 'which': 8 })); // backspace
+        }
+        
+        $s.trigger($.Event('keydown', { 'which': 13 })); // enter
+        continue;
+  
+      } else if (/[\s\t]/.test(data[i])) {
+        data[i] = ' ';
+      }
+  
+      $s.trigger($.Event('keypress', { 'charCode': data.charCodeAt(i) }));
+    }
   };
   
   var createRandomCard = function(name, rows, cols) {
@@ -202,7 +475,23 @@ function rectangleSelect(selector, sx, sy, ex, ey) {
     
     for (var row=[], r=0; r<rows; row=[], r++) {
       for (var c=0; c<cols; c++) {
-        row.push(createCell(color(), color(), Math.floor(Math.random() * 10), Math.random()));
+        row.push(createCell(color(), color(), "@", Math.random()));
+      }
+
+      data.push(row);
+    }
+
+    return createCard(name, data);
+  };
+
+  var createBlankCard = function(name, rows, cols, fg, bg) {
+    var data = [];
+    var fg = fg || "#000";
+    var bg = bg || "#eee";
+    
+    for (var row=[], r=0; r<rows; row=[], r++) {
+      for (var c=0; c<cols; c++) {
+        row.push(createCell(fg, bg, " "));
       }
 
       data.push(row);
@@ -221,14 +510,16 @@ function rectangleSelect(selector, sx, sy, ex, ey) {
     ]);
 
     var randomCard = createRandomCard('card1', 24, 80);
-  
-    var testStack = createStack('stack', 'card1', [
+    var blankCard = createBlankCard('card2', 24, 80);
+
+    var testStack = createStack('stack', 'card2', [
+      blankCard,
       testCard,
       randomCard
     ]);
-
+    
     renderStack($stack, testStack);
-    enableTools($stack, $tools);
+    enableTools($stack, $tools, testStack);
   };
   
   main();
